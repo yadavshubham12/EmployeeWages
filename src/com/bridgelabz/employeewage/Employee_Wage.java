88fe17bb4c35d196c2bcb6ab3705677598d3db46
yadavshubham12 <shubhamyadav5151@gmail.com>
2022-12-28 22:47:09 +0530

    ADDED: Check the Employee Attendance

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -3,5 +3,12 @@ package com.bridgelabz.employeewage;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == 1){
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
