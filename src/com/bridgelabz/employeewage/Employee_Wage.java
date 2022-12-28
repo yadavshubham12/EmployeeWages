a14b833d2100dc56bf8f784e8c6fc3f773fdb579
yadavshubham12 <shubhamyadav5151@gmail.com>
2022-12-29 00:01:58 +0530

    ADDED: Calculate working hours in month

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -5,33 +5,38 @@ import java.lang.invoke.SwitchPoint;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int PARTTIME = 1;
-        int FULLDAY = 2;
-        int WORKINGDAYS = 20;
-        int empWagePerHr = 20;
-        int workingHr = 8;
-        int dailywage = 0;
-        int monthlywage = 0;
-        int FullDayHour = 8;
-        int HalfDayHour = 4;
-        double employeeCheck = Math.floor(Math.random() * 10) % 3;
-
-        switch ((int) employeeCheck) {
-            case 1:
-                System.out.println("Employee is present");
-                System.out.println("Employee is Full Day present");
-                dailywage = empWagePerHr * FullDayHour;
-                System.out.println("Daily employee wage is " +dailywage);
-                monthlywage = dailywage * WORKINGDAYS;
-                System.out.println("Monthly wage is " +monthlywage);
-
-            case 2:
-                System.out.println("Employee is Half Day Present");
-                dailywage = empWagePerHr * HalfDayHour;
-                System.out.println("Daily Half Day Employee Wage is " +dailywage);
-                monthlywage = dailywage * WORKINGDAYS;
-                System.out.println("Monthly wage is " +monthlywage);
-        }
+         int workingDays = 20;
+         int empWagePerHr = 20;
+        int FullDayHr = 8;
+        int HalfDayHr = 4;
+         int maximumHrInMonth = 100;
+        int totalEmployeeHr = 0;
+        int totalWorkingDays = 0;
+        int empHrs;
+        while (totalEmployeeHr <= maximumHrInMonth &&
+                totalWorkingDays < workingDays) {
+            totalWorkingDays++;
+            {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (empCheck) {
+                    case 1:
+                        System.out.println("Employee is full time present");
+                        empHrs = FullDayHr;
+                        break;
+                    case 2:
+                        System.out.println("Employee is part time present");
+                        empHrs = HalfDayHr;
+                        break;
+                    default:
+                        System.out.println("Employee is absent");
+                        empHrs = 0;
+                }
+                totalEmployeeHr = empHrs + totalEmployeeHr;
+                System.out.println("Day" + totalWorkingDays + "EmployeeHr" + empHrs);
+            }
+            int totalEmployeeWage = totalEmployeeHr * empWagePerHr;
+            System.out.println("totalEmployeeWage = " + totalEmployeeWage);
 
         }
     }
+}
