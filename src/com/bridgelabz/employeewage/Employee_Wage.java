0a437e7158a2b673ac14693c4345381135a9a443
yadavshubham12 <shubhamyadav5151@gmail.com>
2022-12-28 23:50:07 +0530

    ADDED: Calculate monthly wages

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -1,43 +1,37 @@
 package com.bridgelabz.employeewage;
 
+import java.lang.invoke.SwitchPoint;
+
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         int PARTTIME = 1;
         int FULLDAY = 2;
+        int WORKINGDAYS = 20;
         int empWagePerHr = 20;
         int workingHr = 8;
         int dailywage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == 1) {
-            dailywage = empWagePerHr * workingHr;
-            System.out.println("Employee is present");
-        } else {
-            System.out.println("Employee is absent");
-            int FullDayHr = 8;
-            int HalfDayHr = 4;
-            int dailyWage = 0;
-            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(empcheck);
+        int monthlywage = 0;
+        int FullDayHour = 8;
+        int HalfDayHour = 4;
+        double employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-            switch (empcheck) {
-                case 1: {
-                    System.out.println("Employee is full day present");
-                    empCheck = empWagePerHr * FullDayHr;
-                }
-                break;
-                case 2: {
-                    System.out.println("Employee is half day present");
-                    empCheck = empWagePerHr + HalfDayHr;
-                }
-                break;
-                case 3: {
-                    System.out.println("Employee is absent");
-                }
-            }
-            System.out.println("Daily wage of employee is : " + dailywage);
-            System.out.println("Employee Wage: " + dailyWage);
+        switch ((int) employeeCheck) {
+            case 1:
+                System.out.println("Employee is present");
+                System.out.println("Employee is Full Day present");
+                dailywage = empWagePerHr * FullDayHour;
+                System.out.println("Daily employee wage is " +dailywage);
+                monthlywage = dailywage * WORKINGDAYS;
+                System.out.println("Monthly wage is " +monthlywage);
+
+            case 2:
+                System.out.println("Employee is Half Day Present");
+                dailywage = empWagePerHr * HalfDayHour;
+                System.out.println("Daily Half Day Employee Wage is " +dailywage);
+                monthlywage = dailywage * WORKINGDAYS;
+                System.out.println("Monthly wage is " +monthlywage);
+        }
 
         }
     }
-}
