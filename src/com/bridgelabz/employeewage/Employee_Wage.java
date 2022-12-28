cccb280b4162769fb91a1aaea2f54b46591a086b
yadavshubham12 <shubhamyadav5151@gmail.com>
2022-12-28 23:16:37 +0530

    ADDED: Print to the Switch Case

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -8,28 +8,36 @@ public class Employee_Wage {
         int empWagePerHr = 20;
         int workingHr = 8;
         int dailywage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == 1) {
+            dailywage = empWagePerHr * workingHr;
             System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
             int FullDayHr = 8;
             int HalfDayHr = 4;
             int dailyWage = 0;
-            double empCheck = Math.floor(Math.random() * 10) % 3;
-            if (empCheck == FULLDAY){
-                FullDayHr = 8;
-                System.out.println("Employee is full day present");
-                dailyWage = empWagePerHr * FullDayHr;
-                System.out.println("Daily wage of employee is : " +dailyWage);
-            }
-            else if (empCheck == PARTTIME){
-                HalfDayHr = 4;
-                System.out.println("Employee is part time present");
-                dailyWage = empWagePerHr * HalfDayHr;
-                System.out.println("Daily wage of employee is : " +dailyWage);
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(empcheck);
 
+            switch (empcheck) {
+                case 1: {
+                    System.out.println("Employee is full day present");
+                    empCheck = empWagePerHr * FullDayHr;
+                }
+                break;
+                case 2: {
+                    System.out.println("Employee is half day present");
+                    empCheck = empWagePerHr + HalfDayHr;
+                }
+                break;
+                case 3: {
+                    System.out.println("Employee is absent");
+                }
             }
-            else {
-                System.out.println("Employee is absent");
-            }
-            System.out.println("Daily wage of employee is : " +dailywage);
+            System.out.println("Daily wage of employee is : " + dailywage);
+            System.out.println("Employee Wage: " + dailyWage);
 
+        }
     }
 }
