0ac66b0f068163dd78ed7fc302859e220ecf218e
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-01 00:08:08 +0530

    ADDED: refactor the code to write class method

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -1,42 +1,37 @@
 package com.bridgelabz.employeewage;
 
-import java.lang.invoke.SwitchPoint;
 
 public class Employee_Wage {
+
+       public static final int PARTTIME = 1;
+       public static final int FULLDAY = 2;
+       public static final int empWagePerHr = 20;
+       public static final int numOfWorkingDays = 2;
+       public static final int workingHr = 8;
+       public static final int maximumHrsMonth = 10;
+       int FullDayHr = 8;
+       int HalfDayHr = 4;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-         int workingDays = 20;
-         int empWagePerHr = 20;
-        int FullDayHr = 8;
-        int HalfDayHr = 4;
-         int maximumHrInMonth = 100;
+        int empHrs = 0;
         int totalEmployeeHr = 0;
         int totalWorkingDays = 0;
-        int empHrs;
-        while (totalEmployeeHr <= maximumHrInMonth &&
-                totalWorkingDays < workingDays) {
+        while (totalEmployeeHr <= maximumHrsMonth && totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
-            {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case 1:
-                        System.out.println("Employee is full time present");
-                        empHrs = FullDayHr;
-                        break;
-                    case 2:
-                        System.out.println("Employee is part time present");
-                        empHrs = HalfDayHr;
-                        break;
-                    default:
-                        System.out.println("Employee is absent");
-                        empHrs = 0;
-                }
-                totalEmployeeHr = empHrs + totalEmployeeHr;
-                System.out.println("Day" + totalWorkingDays + "EmployeeHr" + empHrs);
-            }
-            int totalEmployeeWage = totalEmployeeHr * empWagePerHr;
-            System.out.println("totalEmployeeWage = " + totalEmployeeWage);
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case PARTTIME:
+                    empHrs = 4;
+                    break;
 
+                case FULLDAY:
+                    empHrs = 8;
+                    break;
+            }
+            totalEmployeeHr = empHrs + totalEmployeeHr;
+            System.out.println("Day" + totalWorkingDays + "EmployeeHr" + empHrs);
         }
+        int totalEmployeeWage = totalEmployeeHr * empWagePerHr;
+        System.out.println("totalEmployeeWage = " + totalEmployeeWage);
     }
 }
