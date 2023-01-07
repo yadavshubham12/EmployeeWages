56910a37f278f964acaeaec0c75fad51c3aff8af
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-07 23:42:41 +0530

    ADDED: compute employee wage of multiple company

--- src/com/bridgelabz/employeewage/Employee_Wage.java
+++ src/com/bridgelabz/employeewage/Employee_Wage.java
@@ -1,42 +1,38 @@
 package com.bridgelabz.employeewage;
 
-import java.lang.invoke.SwitchPoint;
+
+import java.util.Locale;
 
 public class Employee_Wage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-         int workingDays = 20;
-         int empWagePerHr = 20;
+    public class EmpWageBuilderOOPS{
+        public static final int PARTTIME = 1;
+        public static final int FULLDAY = 2;
+        public static final int empWagePerHr = 20;
+        public static final int numOfWorkingDays = 2;
+        public static final int workingHr = 8;
+        public static final int maximumHrsMonth = 10;
         int FullDayHr = 8;
         int HalfDayHr = 4;
-         int maximumHrInMonth = 100;
-        int totalEmployeeHr = 0;
-        int totalWorkingDays = 0;
-        int empHrs;
-        while (totalEmployeeHr <= maximumHrInMonth &&
-                totalWorkingDays < workingDays) {
-            totalWorkingDays++;
-            {
-                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
-                    case 1:
-                        System.out.println("Employee is full time present");
-                        empHrs = FullDayHr;
-                        break;
-                    case 2:
-                        System.out.println("Employee is part time present");
-                        empHrs = HalfDayHr;
-                        break;
-                    default:
-                        System.out.println("Employee is absent");
-                        empHrs = 0;
-                }
-                totalEmployeeHr = empHrs + totalEmployeeHr;
-                System.out.println("Day" + totalWorkingDays + "EmployeeHr" + empHrs);
-            }
-            int totalEmployeeWage = totalEmployeeHr * empWagePerHr;
-            System.out.println("totalEmployeeWage = " + totalEmployeeWage);
 
-        }
     }
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee Wage");
+        Employee_Wage empWageBuilderOOPS = new Employee_Wage();
+        empWageBuilderOOPS.computeWage("AMAZON", 20, 20, 100);
+        empWageBuilderOOPS.computeWage("FLIPKART", 20, 20, 100);
+    }
+
+    public void computeWage(String company, int empWagePerHr, int numOfWorkingDays, int maximumHrsMonth) {
+        int empHrs;
+        int empWage = 0;
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+        int totalEmpWage = 0;
+        while (totalEmpHrs <= maximumHrsMonth && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
+        }
+        System.out.println("TOtal employee Wage of empl in " + company + "is" + totalEmpWage);
+
+    }
+
 }
